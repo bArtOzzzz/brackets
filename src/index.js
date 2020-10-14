600eda2d126f9b1501399fc99baf96402178b16e
bArtOzzzz <breakyourbrain55@gmail.com>
2020-10-14 19:06:16 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  module.exports = function check(str, bracketsConfig) {
+    var chars = str.split(''), stack = [], open = [], close = [], closeIndex, openIndex;
+      for (let el of bracketsConfig) { open.push( el[0] );
+        close.push( el[1] ); }
+      for (var i = 0, len = chars.length; i < len; i++) {
+        openIndex = open.indexOf(chars[i]);
+        if (openIndex !== -1) { stack.push(openIndex);
+          continue; }
+        closeIndex = close.indexOf(chars[i]);
+        if (closeIndex !== -1) { openIndex = stack.pop();
+            if (closeIndex !== openIndex) return false;  }
+      }
+      if (stack.length !== 0) return false; 
+      else return true;
+  }
 }
